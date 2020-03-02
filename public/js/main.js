e0748fee13a9f8f56873fcb6615f93fcc6e62917
MohammadShahbaziP <mohammadshahbazi03zn@gmail.com>
2020-03-02 06:21:27 -0800

    setup production

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ const message1 = document.querySelector('#message-1')
 const message2 = document.querySelector('#message-2')
 
 const getForcast = (location) => {
-    fetch("http://localhost:3000/weather?address=" + location).then(res => {
+    fetch("/weather?address=" + location).then(res => {
       res.json().then(data => {
         if (data.error) {
           return message1.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const publicPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
 
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
@@ -81,6 +82,6 @@ app.get('*', (req, res)=> {
     })
 })
 
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
     console.log('server is up!');
 })
